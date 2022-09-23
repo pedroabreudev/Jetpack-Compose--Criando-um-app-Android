9eb62e6fccdb4028b984b8a521cd4f8308c4c5ca
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2022-09-22 21:16:36 -0400

    [Class 03]: 02 - Applying Gradient, Alignment, and Offset.

--- app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
@@ -6,16 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.pedroabreudev.aluvery.ui.theme.AluveryTheme
+import br.com.pedroabreudev.aluvery.ui.theme.Purple200
+import br.com.pedroabreudev.aluvery.ui.theme.Purple500
+import br.com.pedroabreudev.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,17 +41,33 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column {
+    Column(
+        modifier = Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
         Box(
             modifier = Modifier
-                .height(50.dp)
-                .background(Color.Blue)
-                .width(50.dp)
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Purple500, Teal200
+                        )
+                    )
+                )
         )
         Image(
+            modifier = Modifier
+                .size(100.dp)
+                .offset(y = (-50).dp)
+                .clip(shape = CircleShape)
+                .align(CenterHorizontally),
             painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null
-        )
+            contentDescription = null,
+
+            )
         Text(text = "Texto 1")
         Text(text = "Texto 2")
     }
