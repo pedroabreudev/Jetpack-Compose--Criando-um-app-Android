da1701f15a3921278205480b3e7862d934e16750
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2022-09-22 21:31:06 -0400

    [Class 03]: 06 - Removed Imports.

--- app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
@@ -7,17 +7,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.BottomCenter
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -26,7 +22,6 @@ import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.pedroabreudev.aluvery.ui.theme.AluveryTheme
-import br.com.pedroabreudev.aluvery.ui.theme.Purple200
 import br.com.pedroabreudev.aluvery.ui.theme.Purple500
 import br.com.pedroabreudev.aluvery.ui.theme.Teal200
 
