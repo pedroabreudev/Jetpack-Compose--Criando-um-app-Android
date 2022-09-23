c29e562432703845ac59a73606508e13709a6ae2
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2022-09-22 20:42:53 -0400

    [Class 02:] - Created ProductItem

--- app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
@@ -3,13 +3,18 @@ package br.com.pedroabreudev.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.pedroabreudev.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +22,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AluveryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface {
+                    ProductItem()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AluveryTheme {
-        Greeting("Android")
+fun ProductItem() {
+    Column {
+        Box(
+            modifier = Modifier
+                .height(50.dp)
+                .background(Color.Blue)
+                .width(50.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null
+        )
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
     }
 }
