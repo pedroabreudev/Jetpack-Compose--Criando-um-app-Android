e70e9453dee159dcd92ef9396989b88512a4b228
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2022-09-22 21:30:46 -0400

    [Class 03]: 06 - Adjusting the texts.

--- app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/pedroabreudev/aluvery/MainActivity.kt
@@ -12,14 +12,19 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.pedroabreudev.aluvery.ui.theme.AluveryTheme
 import br.com.pedroabreudev.aluvery.ui.theme.Purple200
 import br.com.pedroabreudev.aluvery.ui.theme.Purple500
@@ -57,18 +62,32 @@ fun ProductItem() {
                         )
                     )
                 )
-        )
-        Image(
-            modifier = Modifier
-                .size(100.dp)
-                .offset(y = (-50).dp)
-                .clip(shape = CircleShape)
-                .align(CenterHorizontally),
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null,
-
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(100.dp)
+                    .offset(y = (50).dp)
+                    .clip(shape = CircleShape)
+                    .align(BottomCenter),
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
             )
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = LoremIpsum(50).values.first(),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                modifier = Modifier.padding(top = 8.dp),
+                text = "R$ 14,99",
+                fontSize = 14.sp,
+                fontWeight = FontWeight(400)
+            )
+        }
     }
 }
