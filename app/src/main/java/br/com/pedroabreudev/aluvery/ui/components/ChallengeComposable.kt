46241b3c60a8b57408f649ab4182381599ec941d
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-10-07 14:11:54 -0400

    [Class 03]: 09 - Challenge Composable

--- app/src/main/java/br/com/pedroabreudev/aluvery/ui/components/ChallengeComposable.kt
+++ app/src/main/java/br/com/pedroabreudev/aluvery/ui/components/ChallengeComposable.kt
@@ -1,44 +1,90 @@
 package br.com.pedroabreudev.aluvery.ui.components
 
+import android.util.Log.d
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.pedroabreudev.aluvery.R
+import br.com.pedroabreudev.aluvery.ui.theme.Purple200
+import br.com.pedroabreudev.aluvery.ui.theme.Purple700
 
 @Preview(showBackground = true)
 @Composable
 fun ChallengePreview() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp)
+    Surface(
+        Modifier.padding(8.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
     ) {
-        Box(
-            modifier = Modifier
-                .background(Color.Blue)
-                .width(100.dp)
-                .fillMaxHeight()
-        )
-        Column(
-            modifier = Modifier
+        Row(
+            Modifier
+                .height(height = 200.dp)
                 .fillMaxWidth()
         ) {
-            Text(
-                text = "Test 1",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFFededed))
+            val imageSize = 100.dp
+            Box(
+                Modifier
+                    .fillMaxHeight()
+                    .width(imageSize)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            listOf(Purple700, Purple200)
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .size(imageSize)
+                        .align(Alignment.Center)
+                        .offset(x = imageSize / 2)
+                        .clip(CircleShape)
+                        .border(
+                            BorderStroke(
+                                2.dp, brush = Brush.verticalGradient(
+                                    listOf(
+                                        Purple200, Purple700
+                                    )
+                                )
+                            ), CircleShape
+                        )
+                )
+            }
+            Spacer(modifier = Modifier.width(imageSize / 2))
+            Box(
+                Modifier
                     .padding(16.dp)
-            )
-            Text(
-                text = "Test 2",
-                modifier = Modifier.padding(16.dp)
-            )
+                    .fillMaxHeight()
+                    .padding(16.dp)
+                    .align(CenterVertically)
+            ) {
+                Column {
+                    Text(
+                        text = LoremIpsum(50).values.first(),
+                        overflow = TextOverflow.Ellipsis,
+                        lineHeight = 20.sp
+                    )
+                }
+            }
         }
     }
-
 }
